ac7b1ce84d932ed2dd86f93c7f190871a18de5d8
Jana Ahurtsova <byjankaaa@gmail.com>
2023-07-26 23:24:12 +0300

    feat: add albums

---
+++ src/album/dto/create-album.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsNumber()
+  year?: number;
+  artistId?: string | null;
+}
